9d15448d6ec63558a5ab4af3f9828a9c7041bf1f
Doronin Stanislav <mugisbrows@gmail.com>
2021-10-24 14:50:53 +0300

    0.0.2

---
+++ list.py
@@ -0,0 +1,13 @@
+import os
+
+def main():
+    path = "C:\\Qt"
+    dest = os.path.join(os.path.dirname(__file__), "listing.txt")
+    with open(dest, 'w', encoding='utf-8') as outf:
+        for root, dirs, files in os.walk(path):
+            for f in files:
+                if os.path.splitext(f)[1] in ['.exe']:
+                    outf.write(os.path.join(root, f) + "\n")
+    
+if __name__ == "__main__":
+    main()
